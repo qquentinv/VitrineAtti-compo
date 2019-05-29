f587d72db3d966dd052d41b1b003e7e54c5a0bcf
qquentinv <quentinvoisin00@gmail.com>
2019-05-29 16:41:33 +0200

    Feature #7

--- src/pages/Showcase/index.js
+++ src/pages/Showcase/index.js
@@ -1,52 +1,94 @@
 import React from "react";
-import Imgatti from "../../image/atti-compo.png";
+import Imgatti from "../../image/puzzle.png";
 import { Case } from "./style";
 
 class Showcase extends React.Component {
   render() {
     return (
       <div>
-        <h1 style={{ marginLeft: "200px", color: "#f29400" }}>Showcase</h1>
-        <Case>
-          <div>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <h1 style={{ color: "#f29400" }}>Showcase</h1>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            width: "100%",
+            marginTop: "10px",
+            flexDirection: "row"
+          }}
+        >
+          <Case>
             <div
               style={{
                 display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
                 justifyContent: "center"
               }}
             >
-              <h1 style={{ textAlign: "center" }}>Explain</h1>
+              <img src={Imgatti} alt="atti-compo" />
+              <div>
+                <h1>Explain</h1>
+              </div>
             </div>
-            <br />
+          </Case>
+          <Case>
             <div
               style={{
-                justifyContent: "center",
-                marginLeft: "200px",
-                marginRight: "200px",
-                borderBottom: "1px solid lightgrey"
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center"
               }}
             >
-              <p>
-                Impedita tempestate annonae coalito et more praetorio dignitates
-                ipse ut et ea sit ipse atque causam ea annonae alioqui quam
-                exagitabat alioqui in ad feritas compellebatur dignitates ipse
-                ad ad.Pergunt ita haec cavatis offertur tutius manibus tamen
-                sunt usque quidam ad nobilium Spoletium tutius.
-              </p>
+              <img src={Imgatti} alt="atti-compo" />
+              <div>
+                <h1>Explain</h1>
+              </div>
             </div>
-            <br />
-          </div>
-          <br />
-          <br />
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center"
-            }}
-          >
-            <img src={Imgatti} alt="atti-compo" />
-          </div>
-        </Case>
+          </Case>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            width: "100%",
+            marginTop: "10px",
+            flexDirection: "row"
+          }}
+        >
+          <Case>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center"
+              }}
+            >
+              <img src={Imgatti} alt="atti-compo" />
+              <div>
+                <h1>Explain</h1>
+              </div>
+            </div>
+          </Case>
+          <Case>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center"
+              }}
+            >
+              <img src={Imgatti} alt="atti-compo" />
+              <div>
+                <h1>Explain</h1>
+              </div>
+            </div>
+          </Case>
+        </div>
       </div>
     );
   }
--- src/pages/Showcase/style/index.js
+++ src/pages/Showcase/style/index.js
@@ -3,10 +3,10 @@ import styled from "styled-components";
 export const Case = styled.div`
   background: ${props =>
     props.primary ? "rgb(247, 247, 247)" : "rgb(247, 247, 247)"};
-  margin: 200px;
-  width: 80%;
-  padding: 15px;
-  margin-top: 100px;
+  margin: 50px;
+  width: 40%;
+  padding: 25px;
+  margin-top: 50px;
   border: 1px solid lightgrey;
   border-radius: 10px;
 `;
