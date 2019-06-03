6077cc2cd33780a3e2c41d6670d4cd035d8ee1a5
qquentinv <quentinvoisin00@gmail.com>
2019-06-03 11:38:52 +0200

    Feature #9 : Tuto

--- src/pages/Tutorials/index.js
+++ src/pages/Tutorials/index.js
@@ -1,11 +1,33 @@
 import React from "react";
+import { Content, Li } from "./style";
+import { Container, Header3 } from "atti-components";
 
 class Tutorials extends React.Component {
   render() {
     return (
-      <div>
-        <h1 style={{ marginLeft: "200px", color: "#f29400" }}>Tutorials</h1>
-      </div>
+      <Content>
+        <Container>
+          <h1
+            style={{
+              color: "#f29400",
+              marginBottom: "50px"
+            }}
+          >
+            Tutorials
+          </h1>
+          <div style={{ marginLeft: "150px" }}>
+            <Header3>
+              <ul>
+                <Li>Latius Sericus palaestrita vincula ea</Li>
+                <Li>Latius multitudine repellere educata superabatur</Li>
+                <Li>Per per constrinxerunt membrorum corpora</Li>
+                <Li>Enim Ad difficiles istum honoribus</Li>
+                <Li>Per castra Galli praepotens perferens</Li>
+              </ul>
+            </Header3>
+          </div>
+        </Container>
+      </Content>
     );
   }
 }
---
+++ src/pages/Tutorials/style/index.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Content = styled.div`
+  padding-top: 100px;
+  padding-bottom: 174px;
+`;
+
+export const Li = styled.li`
+  margin-bottom: 45px;
+
+  :hover {
+    color: #f49700;
+  }
+`;
