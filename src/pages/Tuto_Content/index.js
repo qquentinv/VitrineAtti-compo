6c92682368900a18d4787db616e14f438ae62c38
qquentinv <quentinvoisin00@gmail.com>
2019-06-03 13:57:54 +0200

    Feature #9 : Tuto

--- src/pages/Tuto_Content/index.js
+++ src/pages/Tuto_Content/index.js
@@ -1,41 +1,41 @@
 import React from "react";
-import { Content } from "./style";
-import { Container, Header3, Header4, BigText } from "atti-components";
+import { Content, StyledHeader2, StyledHeader3, StyledBigText } from "./style";
+import { Container } from "atti-components";
 
 class Tuto_Content extends React.Component {
   render() {
     return (
       <Content>
         <Container>
-          <Header3>Overall Title</Header3>
-          <Header4>Subtitle 1</Header4>
-          <BigText>
+          <StyledHeader2>Overall Title</StyledHeader2>
+          <StyledHeader3>Subtitle 1</StyledHeader3>
+          <StyledBigText>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
             pellentesque euismod ante, vel facilisis lacus fringilla sed.
             Phasellus ut eros eu urna sagittis sollicitudin vitae quis tortor.
             Vivamus fermentum ac dui vel hendrerit. Phasellus nec urna commodo
             nisl commodo volutpat. Ut viverra tempus augue et porttitor.
             Suspendisse non tincidunt sem. Nullam nec augue urna.
-          </BigText>
-          <Header4>Subtitle 2</Header4>
-          <BigText>
+          </StyledBigText>
+          <StyledHeader3>Subtitle 2</StyledHeader3>
+          <StyledBigText>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
             pellentesque euismod ante, vel facilisis lacus fringilla sed.
             Phasellus ut eros eu urna sagittis sollicitudin vitae quis tortor.
             Vivamus fermentum ac dui vel hendrerit. Phasellus nec urna commodo
             nisl commodo volutpat. Ut viverra tempus augue et porttitor.
             Suspendisse non tincidunt sem. Nullam nec augue urna.
-          </BigText>
+          </StyledBigText>
 
-          <Header4>Subtitle 3</Header4>
-          <BigText>
+          <StyledHeader3>Subtitle 3</StyledHeader3>
+          <StyledBigText>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
             pellentesque euismod ante, vel facilisis lacus fringilla sed.
             Phasellus ut eros eu urna sagittis sollicitudin vitae quis tortor.
             Vivamus fermentum ac dui vel hendrerit. Phasellus nec urna commodo
             nisl commodo volutpat. Ut viverra tempus augue et porttitor.
             Suspendisse non tincidunt sem. Nullam nec augue urna.
-          </BigText>
+          </StyledBigText>
         </Container>
       </Content>
     );
--- src/pages/Tuto_Content/style/index.js
+++ src/pages/Tuto_Content/style/index.js
@@ -1,6 +1,19 @@
 import styled from "styled-components";
+import { Header2, Header3, BigText } from "atti-components";
 
 export const Content = styled.div`
   padding-top: 100px;
   padding-bottom: 174px;
+`;
+
+export const StyledHeader2 = styled(Header2)`
+  margin-bottom: 45px;
+`;
+
+export const StyledHeader3 = styled(Header3)`
+  margin: 25px;
+`;
+
+export const StyledBigText = styled(BigText)`
+  margin: 25px;
 `;
