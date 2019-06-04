3b114679d458300634378437eac59a0aa425087a
qquentinv <quentinvoisin00@gmail.com>
2019-06-04 09:21:17 +0200

    Fix #10

--- src/pages/Shocasies/style/index.js
+++ src/pages/Shocasies/style/index.js
@@ -41,7 +41,7 @@ export const Content = styled.div`
 export const StyledImage = styled.div`
   display: flex;
   position: relative;
-  boxshadow: 0 1px 4px 0 rgba(0, 0, 0, 0.2);
+  box-shadow: 0 1px 4px 0 rgba(0, 0, 0, 0.2);
   margin-bottom: 50px;
 `;
 
