8c3ded413eeb153b2937dcf76dd430389cd86ed4
qquentinv <quentinvoisin00@gmail.com>
2019-06-21 10:17:28 +0200

    Fix #17

--- src/components/Menu/styles/index.js
+++ src/components/Menu/styles/index.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import { Menu } from "atti-components";
 
 export const MenuContainer = styled("div")`
   display: flex;
